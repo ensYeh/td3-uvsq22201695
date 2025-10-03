311e10f45d52eff07bd8ebd8037bce87f5e7c2b9
uvsq22201695 <113375225+uvsq22201695@users.noreply.github.com>
2025-10-03 11:53:36 +0200

    Ajoute les tests échouant pour classe AdresseIp

---
+++ src/test/java/fr/uvsq/cprog/collex/AdresseIpTest.java
@@ -0,0 +1,38 @@
+package fr.uvsq.cprog.collex;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class AdresseIpTest {
+
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "1.1.1.1",
+      "127.0.0.1"
+  })
+  public void testAdresseIP(String ip) {
+    assertEquals(ip, new AdresseIp(ip).toString());
+  }
+
+  @ParameterizedTest
+  @NullSource
+  public void testAdresseIPNull(String ip) {
+    assertThrows(IllegalArgumentException.class, () -> new AdresseIp(ip));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "1-1.1.1",  // Format invalide
+      "127.0.0",  // Format invalide
+      "12a.0.0.1",  // Caractère interdit
+      "456.0.0.1",  // Borne illegal
+      "-356.0.01"  // Borne illegal
+  })
+  public void testAdresseIPIllegal(String ip) {
+    assertThrows(IllegalArgumentException.class, () -> new AdresseIp(ip));
+  }
+}
